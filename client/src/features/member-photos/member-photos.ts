d48e4b5e0f93a02fe40dfbdbc5ee5c39d0aa53d2
MauriRiver345 <106288129+MauriRiver345@users.noreply.github.com>
2026-04-13 17:55:32 -0600

    feat: Parcial05 - Add fix to photo upload

--- client/src/features/member-photos/member-photos.ts
+++ client/src/features/member-photos/member-photos.ts
@@ -42,6 +42,9 @@ export class MemberPhotos implements OnInit {
         this.membersService.editMode.set(false);
         this.loading.set(false);
         this.photos.update(photos => [...photos, photo]);
+        if (!this.membersService.member()?.imageUrl) {
+          this.setMainLocalPhoto(photo);
+        }
       },
       error: error => {
         console.log('Error while uploading the image: ', error);
@@ -53,15 +56,17 @@ export class MemberPhotos implements OnInit {
   setMainPhoto(photo: Photo) {
     this.membersService.setMainPhoto(photo).subscribe({
       next: () => {
-        const currentUser = this.accountService.currentUser();
-        if (currentUser) currentUser.imageUrl = photo.url;
-        this.accountService.setCurrentUser(currentUser as User);
-        this.membersService.member.update(member => ({
-          ...member,
-          imageUrl: photo.url
-        }) as Member);
+        this.setMainLocalPhoto(photo);
       }
     });
   }
-
+  private setMainLocalPhoto(photo: Photo) {
+    const currentUser = this.accountService.currentUser();
+    if (currentUser) currentUser.imageUrl = photo.url;
+    this.accountService.setCurrentUser(currentUser as User);
+    this.membersService.member.update(member => ({
+      ...member,
+      imageUrl: photo.url
+    }) as Member);
+  }
 }
