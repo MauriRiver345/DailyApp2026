6a69670177f93ed75e97c7922dc05588e5219f19
MauriRiver345 <106288129+MauriRiver345@users.noreply.github.com>
2026-04-14 19:09:47 -0600

    feat: Pacial05 - Add more filtering to API

--- src/API/Data/MembersRepository.cs
+++ src/API/Data/MembersRepository.cs
@@ -32,6 +32,10 @@ public class MembersRepository(AppDbContext context) : IMembersRepository
             query = query.Where(x => x.Gender == request.Gender);
         }
 
+        var minAgeDate = DateOnly.FromDateTime(DateTime.Today.AddYears(-request.MaxAge - 1));
+        var maxAgeDate = DateOnly.FromDateTime(DateTime.Today.AddYears(-request.MinAge));
+        query = query.Where(x => x.BirthDay >= minAgeDate && x.BirthDay <= maxAgeDate);
+
         return await Pagination.CreateAsync(query, request.PageNumber, request.PageSize);
     }
 
